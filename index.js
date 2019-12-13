4d27d93070d663a713936bc3baf9197625dd15ef
Matt Haltom <matth@anothercolor.com>
2019-12-13 09:22:53 -0700

    play once

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var animation = lottie.loadAnimation({
   container: document.getElementById('bm'),
   renderer: 'svg',
-  loop: true,
+  loop: false,
   autoplay: true,
   path: 'course-complete.json'
 })
